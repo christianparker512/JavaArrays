aee7ecf1a9df13635369f86f4d7a17c94d504663
Christian Parker <christianparker512@gmail.com>
2021-09-10 14:15:51 -0500

    more arrays

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -1,26 +1,21 @@
 package parker.learningJava;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+        private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        int[]myIntArray = new int[25];
-        //how to define the array and assigning the array
-//	int [] myIntArray = {1,2,3,4,5,6,7,8,9,10};
-//    //saving in element 6;
-//
-//	double[] myDoubleArray = new double[10];
-//        System.out.println(myIntArray[5]);
-        for (int i=0; i<myIntArray.length; i++){
-            myIntArray[i] = i *10;
+        int [] myIntegers = getIntegers(5);
+        for(int i =0; i<myIntegers.length; i++) {
+            System.out.println("Element " + i + ",typed value was " + myIntegers[i]);
         }
-        printArray(myIntArray);
-//        for (int i=0; i<myIntArray.length; i++){
-//            System.out.println("Element " + i + ", value is " + myIntArray[i]);
-//        }
     }
-    public static void printArray(int [] array){
-        for (int i=0; i<array.length; i++){
-       System.out.println("Element " + i + ", value is " + array[i]);
+    public static int [] getIntegers(int number){
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+        for (int i=0; i < values.length; i++){
+            values[i] = scanner.nextInt();
         }
+        return values;
     }
 }
