8b028c9ee9d552b60eb0268d1f572366eb6c3707
Christian Parker <christianparker512@gmail.com>
2021-09-10 12:18:09 -0500

    initializing an array with a for loop

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -3,12 +3,18 @@ package parker.learningJava;
 public class Main {
 
     public static void main(String[] args) {
+        int[]myIntArray = new int[10];
         //how to define the array and assigning the array
-	int [] myIntArray = {1,2,3,4,5,6,7,8,9,10};
-    //saving in element 6;
-
-	double[] myDoubleArray = new double[10];
-        System.out.println(myIntArray[5]);
-
+//	int [] myIntArray = {1,2,3,4,5,6,7,8,9,10};
+//    //saving in element 6;
+//
+//	double[] myDoubleArray = new double[10];
+//        System.out.println(myIntArray[5]);
+        for (int i=0; i<10; i++){
+            myIntArray[i] = i *10;
+        }
+        for (int i=0; i<10; i++){
+            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        }
     }
 }
