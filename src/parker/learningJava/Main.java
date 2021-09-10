70fd4b51520bc0b3afb5f26bdb54f2e7d1bd45fe
Christian Parker <christianparker512@gmail.com>
2021-09-10 14:19:50 -0500

    take in five numbers and get the average

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -9,6 +9,7 @@ public class Main {
         for(int i =0; i<myIntegers.length; i++) {
             System.out.println("Element " + i + ",typed value was " + myIntegers[i]);
         }
+        System.out.println("The average is " + getAverage(myIntegers));
     }
     public static int [] getIntegers(int number){
         System.out.println("Enter " + number + " integer values.\r");
@@ -17,5 +18,12 @@ public class Main {
             values[i] = scanner.nextInt();
         }
         return values;
+    }
+    public static double getAverage(int [] array){
+        int sum = 0;
+        for(int i =0; i<array.length; i++){
+            sum += array[i];
+        }
+        return (double) sum /(double)array.length;
     }
 }
