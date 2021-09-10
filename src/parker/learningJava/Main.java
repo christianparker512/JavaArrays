9c8c28a5c87f2d1b3f3365f57fe163bd374643a5
Christian Parker <christianparker512@gmail.com>
2021-09-10 12:12:19 -0500

    define and assign elements in the array

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -3,8 +3,12 @@ package parker.learningJava;
 public class Main {
 
     public static void main(String[] args) {
-	int [] myVariable;
-	myVariable = new int[10];
+        //how to define the array and assigning the array
+	int [] myIntArray = new int[10];
+    //saving in element 6;
+	myIntArray [5] = 50;
+	double[] myDoubleArray = new double[10];
+        System.out.println(myIntArray[5]);
 
     }
 }
