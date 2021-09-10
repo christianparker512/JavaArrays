3ded00c32b64719dc6a9b51c6640bbd07c9bb96b
Christian Parker <christianparker512@gmail.com>
2021-09-10 12:52:51 -0500

    length array in the forloop

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -3,17 +3,17 @@ package parker.learningJava;
 public class Main {
 
     public static void main(String[] args) {
-        int[]myIntArray = new int[10];
+        int[]myIntArray = new int[25];
         //how to define the array and assigning the array
 //	int [] myIntArray = {1,2,3,4,5,6,7,8,9,10};
 //    //saving in element 6;
 //
 //	double[] myDoubleArray = new double[10];
 //        System.out.println(myIntArray[5]);
-        for (int i=0; i<10; i++){
+        for (int i=0; i<myIntArray.length; i++){
             myIntArray[i] = i *10;
         }
-        for (int i=0; i<10; i++){
+        for (int i=0; i<myIntArray.length; i++){
             System.out.println("Element " + i + ", value is " + myIntArray[i]);
         }
     }
