28257fdaf5748dbe71bc4e77b3deea187ff05b71
Christian Parker <christianparker512@gmail.com>
2021-09-10 14:03:09 -0500

    filling an array with array.length

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -13,8 +13,14 @@ public class Main {
         for (int i=0; i<myIntArray.length; i++){
             myIntArray[i] = i *10;
         }
-        for (int i=0; i<myIntArray.length; i++){
-            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        printArray(myIntArray);
+//        for (int i=0; i<myIntArray.length; i++){
+//            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+//        }
+    }
+    public static void printArray(int [] array){
+        for (int i=0; i<array.length; i++){
+       System.out.println("Element " + i + ", value is " + array[i]);
         }
     }
 }
