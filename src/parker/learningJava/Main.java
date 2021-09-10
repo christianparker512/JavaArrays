549065cfa80acd96a9b9af1f270cc7638c385cf4
Christian Parker <christianparker512@gmail.com>
2021-09-10 12:14:28 -0500

    initializing the array

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         //how to define the array and assigning the array
-	int [] myIntArray = new int[10];
+	int [] myIntArray = {1,2,3,4,5,6,7,8,9,10};
     //saving in element 6;
-	myIntArray [5] = 50;
+
 	double[] myDoubleArray = new double[10];
         System.out.println(myIntArray[5]);
 
